8fdda7c03df69bf655c80848939031874b3126e6
santi3870 <general.worker1975@gmail.com>
2024-12-16 09:38:58 +0700

    new test_v2

--- script.js
+++ script.js
@@ -1,2 +1,58 @@
-console.log("santi lungaor");
-console.log("Bbas");
+let currentInput = '';
+let currentOperation = '';
+let previousInput = '';
+
+function appendNumber(number) {
+  currentInput += number;
+  updateDisplay();
+}
+
+function clearDisplay() {
+  currentInput = '';
+  previousInput = '';
+  currentOperation = '';
+  updateDisplay();
+}
+
+function setOperation(operation) {
+  if (currentInput === '') return;
+  if (previousInput !== '') {
+    calculateResult();
+  }
+  currentOperation = operation;
+  previousInput = currentInput;
+  currentInput = '';
+}
+
+function calculateResult() {
+  let result;
+  const prev = parseFloat(previousInput);
+  const current = parseFloat(currentInput);
+  if (isNaN(prev) || isNaN(current)) return;
+
+  switch (currentOperation) {
+    case '+':
+      result = prev + current;
+      break;
+    case '-':
+      result = prev - current;
+      break;
+    case '*':
+      result = prev * current;
+      break;
+    case '/':
+      result = prev / current;
+      break;
+    default:
+      return;
+  }
+
+  currentInput = result.toString();
+  currentOperation = '';
+  previousInput = '';
+  updateDisplay();
+}
+
+function updateDisplay() {
+  document.getElementById('display').value = currentInput;
+}
